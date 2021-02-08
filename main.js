2b4425862e6f0999e6642562b8a7f4626ca82e53
Mming <mingming15256@gmail.com>
2021-02-08 16:04:16 +0700

    add assert

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 function sum(a,b){
+    if(Array.isArray(a)){
+        let total =0;
+        for(let i=0;i<a.length;i++)
+            total += a[i]
+        
+        return total
+    }
     return a+b
 }
 
--- main.test.js
+++ main.test.js
@@ -1,5 +1,4 @@
 const math = require('./main')
 const assert = require('assert')
 
-console.log(math.sum(2,2))
-assert.strictEqual(math.sum(2,2),4)
+assert.strictEqual(math.sum([1,2,3,4,5]),15)
